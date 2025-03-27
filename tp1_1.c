fc966132226e7ac8ec7c5ef9e1f30fb5271d6b5b
Joaquin <joaquinlechesi@gmail.com>
2025-03-27 00:23:26 -0300

    segundo commit

--- tp1_1.c
+++ tp1_1.c
@@ -3,5 +3,16 @@
 int main()
 {
     printf("hola mundo");
+
+    //e)
+    int numero = 1;
+    int *p_numero = &numero;
+
+    printf("\nEl contenido del puntero es: %p\n", p_numero);
+    printf("\nLa direcion de memoria almacenada por el puntero es: %p\n", p_numero);
+    printf("\nLa direcion de memoria de la variable es: %p\n", &numero);
+    printf("\nLa direcion de memoria del puntero es: %p\n", &p_numero);
+    printf("\nEl tamanio de memoria utilizado por la variable es: %zu\n", sizeof(numero));
+    
     return 0;
 }
